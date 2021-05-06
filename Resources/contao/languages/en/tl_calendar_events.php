f265f29fc94ab1acc9bb37f541bf51845bf01300
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2021-05-06 23:22:02 +0200

    Interim status for con4gis 8

--- Resources/contao/languages/en/tl_calendar_events.php
+++ Resources/contao/languages/en/tl_calendar_events.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * This file is part of con4gis,
- * the gis-kit for Contao CMS.
- *
- * @package   	con4gis
- * @version    7
- * @author  	    con4gis contributors (see "authors.txt")
- * @license 	    LGPL-3.0-or-later
- * @copyright 	Küstenschmiede GmbH Software & Design
- * @link              https://www.con4gis.org
- *
+/*
+ * This file is part of con4gis, the gis-kit for Contao CMS.
+ * @package con4gis
+ * @version 8
+ * @author con4gis contributors (see "authors.txt")
+ * @license LGPL-3.0-or-later
+ * @copyright (c) 2010-2021, by Küstenschmiede GmbH Software & Design
+ * @link https://www.con4gis.org
  */
 
 $GLOBALS['TL_LANG']['tl_calendar_events']['c4gEditEvent'] = ['Event <=> Reservation', 'Here you can link events and reservations.'];
