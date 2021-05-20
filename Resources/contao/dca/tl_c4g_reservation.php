6b48c3fa218f36bb81a9f3c215abbb8201ea8557
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2021-05-20 14:42:20 +0200

    Added reservation type filter

--- Resources/contao/dca/tl_c4g_reservation.php
+++ Resources/contao/dca/tl_c4g_reservation.php
@@ -198,6 +198,7 @@ $GLOBALS['TL_DCA']['tl_c4g_reservation'] = array
         (
             'label'             => &$GLOBALS['TL_LANG']['tl_c4g_reservation']['reservation_type'],
             'inputType'         => 'select',
+            'filter'            => true,
             'exclude'           => true,
             'foreignKey'        => 'tl_c4g_reservation_type.caption',
             'eval'              => array('mandatory' => true, 'tl_class' => 'long'),
