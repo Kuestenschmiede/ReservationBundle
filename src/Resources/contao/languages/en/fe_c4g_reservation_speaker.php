cb72fb78808f8ffea6ab7587956dccc676cff70e
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-21 14:24:04 +0100

    add en lang file

---
+++ src/Resources/contao/languages/en/fe_c4g_reservation_speaker.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of con4gis, the gis-kit for Contao CMS.
+ * @package con4gis
+ * @version 8
+ * @author con4gis contributors (see "authors.txt")
+ * @license LGPL-3.0-or-later
+ * @copyright (c) 2010-2022, by Küstenschmiede GmbH Software & Design
+ * @link https://www.con4gis.org
+ */
+
+/** CONFIGURATION */
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['brick_caption']    = 'Speaker';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['brick_caption_plural'] = 'Speakers';
+
+/** FIELDS */
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['title'] = 'Title';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['firstname'] = 'Firstname';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['lastname'] = 'Lastname';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['email'] = 'Email';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['phone'] = 'Phone';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['address'] = 'Street';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['postal'] = 'Postal';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['city'] = 'City';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['website'] = 'Website';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['vita'] = 'Vita';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['photo'] = 'Photo';
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['events'] = 'Events';
+
+
+/** BUTTONS */
+$GLOBALS['TL_LANG']['fe_c4g_reservation_speaker']['button_cancel'] = 'To list';
